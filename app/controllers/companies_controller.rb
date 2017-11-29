4447c4653532d89287ef6178dae64467d45a7d23
Jay Lockridge <jklockri@pnw.edu>
2017-11-29 11:03:16 -0600

    Add alphabetized list of companies and nav bar

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,8 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all
   end
+
+  def alphabetically
+    @companies = Company.all.order(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/companies/index' => 'companies#index'
+  get '/companies/alphabetically' => 'companies#alphabetically'
 end
