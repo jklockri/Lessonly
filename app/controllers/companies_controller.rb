c0ce2483609a02db496d82115e0c8b9f1b1fa723
Jay Lockridge <jklockri@pnw.edu>
2017-11-29 11:36:14 -0600

    Add modern plan and partial for table

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,4 +6,8 @@ class CompaniesController < ApplicationController
   def alphabetically
     @companies = Company.all.order(:name)
   end
+
+  def with_modern_plan
+    @companies = Company.where.not('plan_level = ?', 'custom').where.not('plan_level =?','legacy')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/companies/index' => 'companies#index'
   get '/companies/alphabetically' => 'companies#alphabetically'
+  get '/companies/with_modern_plan' => 'companies#with_modern_plan'
 end
